b82b862005a936ffdf2d9addba3efe6c83b88201
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-09-11 15:03:47 +0500

    Task3

---
+++ Tyuiu.ShelomentsevYA.Sprint0.Task3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShelomentsevYA.Sprint0.Task3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShelomentsevYA.Sprint0.Task3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c7afe03-f529-459b-a044-147b20a54918")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
